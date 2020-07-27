9000059f2326552e15dc4a8f8b8f517bfb51974a
Victor <corason@gmail.com>
2020-07-27 18:58:57 +0300

    dz 27-07-2020

--- src/components/Ceiling.js
+++ src/components/Ceiling.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Ceiling.css';
 import Cell from './Cell.js';
 
-const cellsQty = 15; // Cells quantity
+const cellsQty = 11; // Cells quantity
 
 function Ceiling() {
   
@@ -12,8 +12,6 @@ function Ceiling() {
   for (let i = 0; i < cellsQty; i++) {
     cellKeys[i] = `cell${i}`;
   }
-
-  // console.log('ceiling allState:',allState); 
 
   const arrCells = cellKeys.map((item) => (
     <Cell key={item} value={allState} />
